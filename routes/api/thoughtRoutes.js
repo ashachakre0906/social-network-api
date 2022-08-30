8543cba2e9ea6ba4a0d967cdf349725e6e87e8d2
ashachakre0906 <chourpagar.asha@gmail.com>
2022-08-30 15:25:57 -0700

    modularized thoughtRoutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,4 +10,23 @@ const {
 
 } = require('../../controllers/thought-controller');
 
+//get and post routes for thoughts
+router.route('/')
+      .get(getAllThoughts)
+      .post(createThought);
+
+//get by id, put and delete routes for thoughts
+router.route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought)
+
+//add reaction
+router.route('/:thoughtId/reactions')
+      .post(addReaction)
+
+//remove a reaction
+router.route('/:thoughtId/reactions/:reactionId')
+      .delete(removeReaction);
+      
 module.exports = router;
